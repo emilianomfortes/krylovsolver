7c28768306fb68d5df1405756267dfa7e9344aef
Emi <76208994+emilianomfortes@users.noreply.github.com>
2021-09-01 22:28:19 -0300

    Update utils.py
    Adding paper error bound

--- PyKrylovsolver/utils.py
+++ PyKrylovsolver/utils.py
@@ -47,7 +47,7 @@ def illinois_algorithm(f, a, b, y, margin=.00_001):
             stagnant = 1
 
 
-def optimizer(T, krylov_basis, tlist, tol):
+def optimizer(T, krylov_basis, tlist, tol, method="accuracy"):
     f = bound_function(T, krylov_basis=krylov_basis, t0=tlist[0], tf=tlist[-1])
     n = illinois_algorithm(f, a=tlist[0], b=tlist[-1], y=np.log10(tol), margin=0.1)
     return n
@@ -58,24 +58,46 @@ def bound_function(T, krylov_basis, t0, tf):
     U1 = np.matmul(krylov_basis[0:, 0:].T, eigenvectors1)
     e01 = eigenvectors1.conj().T[:, 0]
 
-    eigenvalues2, eigenvectors2 = eigh(T[0:-1, 0:T.shape[1] - 1])
-    U2 = np.matmul(krylov_basis[0:-1, :].T, eigenvectors2)
-    e02 = eigenvectors2.conj().T[:, 0]
+    if method == "accuracy":
+        eigenvalues2, eigenvectors2 = eigh(T[0:-1, 0:T.shape[1] - 1])
+        U2 = np.matmul(krylov_basis[0:-1, :].T, eigenvectors2)
+        e02 = eigenvectors2.conj().T[:, 0]
+    
+        def f(t):
+            delta_t = -1j * (t - t0)
 
-    def f(t):
-        delta_t = -1j * (t - t0)
+            aux1 = np.multiply(np.exp(delta_t * eigenvalues1), e01)
+            psi1 = np.matmul(U1, aux1)
 
-        aux1 = np.multiply(np.exp(delta_t * eigenvalues1), e01)
-        psi1 = np.matmul(U1, aux1)
+            aux2 = np.multiply(np.exp(delta_t * eigenvalues2), e02)
+            psi2 = np.matmul(U2, aux2)
 
-        aux2 = np.multiply(np.exp(delta_t * eigenvalues2), e02)
-        psi2 = np.matmul(U2, aux2)
+            error = np.linalg.norm(psi1 - psi2)
 
-        error = np.linalg.norm(psi1 - psi2)
-
-        steps = 1 if t == t0 else max(1, tf // (t - t0))
-        return np.log10(error) + np.log10(steps)
-
+            steps = 1 if t == t0 else max(1, tf // (t - t0))
+            return np.log10(error) + np.log10(steps)
+ 
+    if method == "loschmidt_echo":
+        alphas = np.diagonal(T, offset=0).real
+        betas = np.diagonal(T, offset=1).real
+        alpha_mean = np.mean(alphas)
+        beta_mean = np.mean(betas)
+        
+        _dim = T.shape[0]
+        psi_extend = np.zeros(_dim + 1, dtype=complex)
+        psi_extend[0] = 1 + 0*1j
+        
+        T_extend = np.zeros((_dim+1, _dim+1), dtype=complex)
+        T_extend[:_dim, :_dim] = T
+        T_extend[-2, -1] = beta_mean
+        T_extend[-1, -2] = beta_mean
+        T_extend[-1, -1] = alpha_mean
+        
+        eigenvalues2, eigenvectors2 = eigh(T_extend)
+        U2 = np.exp(np.outer(-1j * eigenvalues, tlist))
+        e02 = eigenvectors2.conj().T[:, 0]
+        def f(t):
+            delta_t = -1j * (t - t0)
     return f
 
 
